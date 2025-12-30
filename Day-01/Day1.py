823f3d91801de5117bef54fb3e07afeeb640fcfd
sohamrustagi <sohamrustagi2006@gmail.com>
2025-12-30 21:59:15 +0530

    Update Day1.py

--- Day-01/Day1.py
+++ Day-01/Day1.py
@@ -1,2 +1,14 @@
-a = 7.4
-print("b")
+#Day 1 : python basic 
+#Topics: variables input output type conversion basic arithmetic 
+#program  Print hello word 
+print("Hello World")
+#program taking input 
+input("enter your name")
+#declare a variable and print it 
+a = 12
+print(a)
+#basic arithmetic
+sum_result = num1+num2
+print("sum of two numbers is ",sum_result)
+
+
